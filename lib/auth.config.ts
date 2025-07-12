78c3a898b68bc21a985e4f83b47d0da660c04a21
Gaurav Kumar <gaurav989267@gmail.com>
2025-07-12 23:58:31 +0530

    Update auth.config.ts

--- lib/auth.config.ts
+++ lib/auth.config.ts
@@ -5,7 +5,7 @@ import Google from "next-auth/providers/google";
 import type { NextAuthConfig } from "next-auth";
 // import { LoginSchema } from "@/schemas/AuthSchema";
 // import { getUserByEmail } from "@/data/user";
-// Notice this is only an object, not a full Auth.js instance
+
 export default {
   providers: [
     Google({
