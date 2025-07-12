18ddca5cb624152a56d312932e3f4ee02c4feb12
Gaurav Kumar <gaurav989267@gmail.com>
2025-07-12 23:47:29 +0530

    remove webpack error

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,86 +1,86 @@
 import NextAuth from "next-auth";
 
 import authConfig from "@/lib/auth.config";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import  prisma  from "@/lib/prisma";
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+// import  prisma  from "@/lib/prisma";
 
-import { getUserById } from "@/data/user";
+// import { getUserById } from "@/data/user";
 
-async function testprisma() {
-  try {
-    await prisma.$connect();
-    console.log("Database connected successfully!");
-  } catch (error) {
-    console.error("Database connection failed:", error);
-  }
-}
+// async function testprisma() {
+//   try {
+//     await prisma.$connect();
+//     console.log("Database connected successfully!");
+//   } catch (error) {
+//     console.error("Database connection failed:", error);
+//   }
+// }
 
-testprisma();
+// testprisma();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
-    signIn: "/auth/login",
+    signIn: "/",
     error: "/auth/error",
   },
-  events: {
-    async linkAccount({ user }) {
-      const email = user.email?.split("@")[0];
-      await prisma.user.update({
-        where: { id: user.id },
-        data: { emailVerified: new Date(), userName: email },
-      });
-    },
-  },
-  callbacks: {
-    async signIn({ user, account }) {
-      if (!user.id) return false;
+  // events: {
+  //   async linkAccount({ user }) {
+  //     const email = user.email?.split("@")[0];
+  //     await prisma.user.update({
+  //       where: { id: user.id },
+  //       data: { emailVerified: new Date(), userName: email },
+  //     });
+  //   },
+  // },
+  // callbacks: {
+  //   async signIn({ user, account }) {
+  //     if (!user.id) return false;
 
-      if (account?.provider !== "credentials") {
-        return true;
-      }
+  //     if (account?.provider !== "credentials") {
+  //       return true;
+  //     }
 
-      /***
-       * if user use credentials to login
-       * Prevent sign in without email verificaton
-       */
-      if (account?.provider === "credentials") {
-        const existingUser = await getUserById(user.id);
+  //     /***
+  //      * if user use credentials to login
+  //      * Prevent sign in without email verificaton
+  //      */
+  //     if (account?.provider === "credentials") {
+  //       const existingUser = await getUserById(user.id);
 
-        if (!existingUser?.emailVerified) return false;
-      }
-      return true;
-    },
+  //       if (!existingUser?.emailVerified) return false;
+  //     }
+  //     return true;
+  //   },
 
-    async session({ token, session }) {
-      if (token.sub && session.user) {
-        session.user.id = token.sub;
-      }
+  //   async session({ token, session }) {
+  //     if (token.sub && session.user) {
+  //       session.user.id = token.sub;
+  //     }
 
-      if (token.userName && session.user) {
-        session.user.userName = token.userName;
-      }
+  //     if (token.userName && session.user) {
+  //       session.user.userName = token.userName;
+  //     }
      
 
-      return session;
-    },
+  //     return session;
+  //   },
 
-    async jwt({ token }) {
-      if (!token.sub) return token;
+  //   async jwt({ token }) {
+  //     if (!token.sub) return token;
 
-      const existingUser = await getUserById(token.sub);
+  //     const existingUser = await getUserById(token.sub);
 
-      if (!existingUser) return token;
+  //     if (!existingUser) return token;
 
-      if (existingUser.userName !== null) {
-        token.userName = existingUser.userName;
-      }
+  //     if (existingUser.userName !== null) {
+  //       token.userName = existingUser.userName;
+  //     }
 
     
 
-      return token;
-    },
-  },
-  adapter: PrismaAdapter(prisma),
+  //     return token;
+  //   },
+  // },
+  // adapter: PrismaAdapter(prisma),
   session: {
     strategy: "jwt",
   },
